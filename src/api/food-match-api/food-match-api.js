1bfbf5bb9d3c06fad6b0c0eddc80555bac7633c2
Dante <dantexcortez@gmail.com>
2022-11-08 13:54:01 -0500

    sample data

--- src/api/food-match-api/food-match-api.js
+++ src/api/food-match-api/food-match-api.js
@@ -1,5 +1,72 @@
 const express = require('express')
 const router = express.Router()
+//import sampleRestaurant from './testData/data.js'
+//const sampleRestaurant = require('./testData/data.js')
+//import { sampleRestaurant } from './testData/data.js';
+//const sampleRestaurant = require('./')
+
+const sampleRestaurant = 
+    {
+    'restaurantName': 'aRestaurant',
+    'address': '1 East Pleasant Street',
+    'phoneNumber':'777-777-7777',
+    'Website':'www.restaurant.com',
+    'Menu' : {
+        'Appetizers':
+        {
+            'Vegetarians':
+                [
+                    'Chic’n wings'
+                ],
+
+            'Vegan':
+                [
+                    'Salad'
+                ],
+            'Glutenfree':
+                [
+                    'Salad'
+                ]
+        },
+        'Entree':
+        {
+            'Vegetarians':
+                [
+                    'Chic’n wings'
+                ],
+            'Vegan':
+                [
+                    'Salad'
+                ],
+            
+            'Glutenfree':
+                [
+                    'Glutenfree'
+                ]
+    
+        },
+        'Drinks':
+        {
+            'Vegetarians':
+            [
+                'water'
+            ],
+        
+            'Vegan':
+            [
+                'water'
+            ],
+            'Glutenfree':
+            [
+                'water'
+            ]
+        }
+    }
+    
+}
+
+
+
 
 // middleware that is specific to this router
 router.use((req, res, next) => {
@@ -8,7 +75,8 @@ router.use((req, res, next) => {
 })
 // define the home page route
 router.get('/', (req, res) => {
-  res.send('Foodmatch home page')
+  const json = JSON.stringify(sampleRestaurant);
+  res.send(json);
 })
 // define the about route
 router.get('/about', (req, res) => {
