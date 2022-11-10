f3be16531e226206c9f1061672b749d7c026e3f8
rsonawane2002 <rsonawane2002@gmail.com>
2022-11-09 23:14:20 -0500

    Milestone2

--- src/api/resources/mainscreencontroller.js
+++ src/api/resources/mainscreencontroller.js
@@ -15,7 +15,7 @@ const button = document.getElementById("find");
 console.log(button);
 button.addEventListener("click", () => {
   document.getElementById('table').innerHTML = '';
-  (async ()=>{ const data = await fetch("https://cs326-theta.herokuapp.com/api/restaurant")
+  (async ()=>{ const data = await fetch("https://cs326-theta.herokuapp.com/api/restaurant");
   if(data.ok)
   {
     restaurant = await data.json();
@@ -24,12 +24,14 @@ button.addEventListener("click", () => {
   {
     console.log("Error");
   }
+  console.log(restaurant);
   const name = restaurant.restaurantName;
   const selected = document.getElementById('table');
 
   let temp2 = document.createElement("a");
   temp2.classList = "list-group-item";
-  temp2.href = "RestaurantForm.html";
+  temp2.href = "RestaurantView.html";
+  temp2.id = "1";
   temp2.innerHTML = name;
   document.getElementById("find").remove();
   selected.appendChild(temp2);
